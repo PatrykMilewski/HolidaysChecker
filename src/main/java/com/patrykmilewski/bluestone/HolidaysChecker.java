366bd1ad66ea6e1fb6fd354ecab13bed447a5a5e
Patryk Milewski <pmilewski@bluesoft.net.pl>
2019-04-02 10:20:16 +0200

    Initial commit, created with Spring Initializer
    Used modules: Web, Tests, Lombok, RESTDocs

---
+++ src/main/java/com/patrykmilewski/bluestone/HolidaysChecker.java
@@ -0,0 +1,13 @@
+package com.patrykmilewski.bluestone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidaysChecker {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(HolidaysChecker.class, args);
+    }
+    
+}
---
+++ src/test/java/com/patrykmilewski/bluestone/HolidaysCheckerTests.java
@@ -0,0 +1,16 @@
+package com.patrykmilewski.bluestone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HolidaysCheckerTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
